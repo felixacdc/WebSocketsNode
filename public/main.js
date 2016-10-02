dc7494be965c829b20f6950f504df30c630952f2
felixacdc <pfmata01@gmail.com>
2016-10-02 10:38:10 -0600

    [modify] finished example :+1:

--- public/main.js
+++ public/main.js
@@ -5,11 +5,26 @@ socket.on('messages', function (data) {
     render(data);
 });
 
-function render (data) {
-    var html = `<div>
-        <strong>${data.author}</strong>
-        <em>${data.text}</em>
-    </div>`;
-    
+function render(data) {
+
+    var html = data.map(function (data, index) {
+        return (`<div>
+                    <strong>${data.author}</strong>
+                    <em>${data.text}</em>
+                </div>`);
+    }).join(" ");
+
     document.getElementById("messages").innerHTML = html;
+}
+
+function addMessage(e) {
+    var payload = {
+        author: document.getElementById('username').value,
+        text: document.getElementById('text').value
+    };
+
+    socket.emit('new-message', payload);
+    document.getElementById('username').value = "";
+    document.getElementById('text').value = "";
+    return false;
 }
--- server/main.js
+++ server/main.js
@@ -3,6 +3,12 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var messages = [{
+        id: 1,
+        text: 'Hola soy a todos =)',
+        author: 'Félix Méndez'
+    }];
+
 app.use(express.static('public'));
 
 app.get('hello', function (req, res) {
@@ -11,10 +17,11 @@ app.get('hello', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('Alguien se ha conectado con Socket');
-    socket.emit('messages', {
-        id: 1,
-        text: 'Hola soy un mensaje',
-        author: 'Félix Méndez'
+    socket.emit('messages', messages);
+    
+    socket.on('new-message', function (data) {
+        messages.push(data);
+        io.sockets.emit("messages", messages);
     });
 });
 
